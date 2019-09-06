ea6d3ff8a0e1c8011e7bfe87ad17c98b3f23287c
tarasowski <dimitri+github@tarasowski.de>
2019-09-06 17:22:01 +0200

    module test

---
+++ samples/modules/main.py
@@ -0,0 +1,12 @@
+from utils import my_module
+from utils.sample import module as sm, functions as f
+
+
+res = my_module.main_module()
+res_ = sm.main_sample()
+res__ = f.sum_()
+
+print(res)
+print(res_)
+print(res__)
+
